74c6e2bc873914dc3de291fabd3e0ffbd82b9052
Angel Ricardo Nieto García <angelricardoh@gmail.com>
2020-08-29 19:39:16 -0700

    Almost done with section 3

--- 2211798052.py
+++ 2211798052.py
@@ -4,9 +4,47 @@ import numpy as np
 import os
 # import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from numpy import linalg as LA
 
 IMAGE_SCOPE_SIZE = 800
+c = 10
 
+def make_weights(k, distances):
+  result = np.zeros(k, dtype=np.float32)
+  sum = 0.0
+  for i in range(k):
+    result[i] += 1.0 / distances[i]
+    sum += result[i]
+  result /= sum
+  return result
+
+def KNN(k, X, y, x):
+    N, _ = X.shape
+    num_classes = len(np.unique(y))
+    distances = np.zeros((N,1))
+    for i in range(N):
+        distances[i] = np.linalg.norm(X[i] - x)
+
+    flatten_distances = distances.flatten()
+    sorted_distances = np.argsort(flatten_distances)
+
+    votes = np.zeros(num_classes, dtype=np.float32)
+    classes = y[np.argsort(flatten_distances)][:k]
+
+    k_near_dists = np.zeros(k, dtype=np.float32)    
+    for i in range(k):
+        idx = sorted_distances[i]
+        print("distance = %0.4f" % distances[idx])
+        k_near_dists[i] = distances[idx] # save dists
+        # k_near_dists[i] = np.power(distances[idx] - classes[i], 2)
+        # k_near_dists[i] = LA.norm(x - classes[i], 2)
+
+    wts = make_weights(k, k_near_dists)
+
+    for c in np.unique(classes):
+        votes[c] = wts[i] * 1.0
+
+    return np.argmax(votes)
 
 if __name__ == '__main__':
     # K = sys.argv[1]
@@ -79,6 +117,17 @@ if __name__ == '__main__':
     transformed_training_set_x = pca.fit_transform(training_set_x)
     transformed_test_set_x = pca.transform(test_set_x)
 
-    print(transformed_test_set_x[0])
+    # Hint
+    # print(transformed_test_set_x[0])
 
-    print("end of main function")
+    # Section 3
+    ypred = []
+    # for index in range(N):
+    #     data = np.array([transformed_test_set_x[index], test_set_y[index]])
+    #     KNN(K, transformed_test_set_x, test_set_y, data)
+
+    for x in transformed_test_set_x:
+        ypred.append(KNN(K, transformed_training_set_x, training_set_y, x))
+
+    for index_pred in range(N):
+        print(str(ypred[index_pred]) + " " + str(test_set_y[index_pred]))
